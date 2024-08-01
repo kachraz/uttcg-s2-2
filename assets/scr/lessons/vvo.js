6e8ac617750c712e3b7d32b7f2c4e3bfb6cd9c30
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-01 17:01:52 +0000

    👃👙

--- assets/scr/lessons/vvo.js
+++ assets/scr/lessons/vvo.js
@@ -11,3 +11,11 @@ All the things in the apps are data that needs to be handled
 */
 
 console.log("HelloPany");
+
+// Creatig values
+/*
+1. Variable 1st letter cant be a number
+*/
+
+let message = "howAreYou";
+console.log(message);
