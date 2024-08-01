55dc6d5997f80932345502349b0b1fe6ac8825d5
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-01 19:43:34 +0000

    👃👙

--- assets/scr/lessons/ara.js
+++ assets/scr/lessons/ara.js
@@ -6,3 +6,15 @@ Arrays
 
 const content = ["hey", "heys"];
 console.log(content[0]);
+
+content.push("working");
+
+console.table(content);
+
+const index = content.findIndex((i) => i === "soporrts");
+console.log(index);
+
+const hoii = content.map((i) => ({
+  text: "item",
+}));
+console.log(hoii);
