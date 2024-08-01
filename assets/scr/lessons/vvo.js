90cf2d66081b350b69a26409726ce77240b3a524
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-01 14:57:03 +0000

    👃👙

---
+++ assets/scr/lessons/vvo.js
@@ -0,0 +1,13 @@
+// Valariables Values and Operators
+console.log(`
+===
+Variables, Values and Operators
+===
+`);
+
+// Foundational concepts
+/*
+All the things in the apps are data that needs to be handled
+*/
+
+console.log("HelloPany");
