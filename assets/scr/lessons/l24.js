2a4ff68636a516456e20c48f0d43b70adb1b1dde
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-07 14:13:38 +0000

    👃👙

---
+++ assets/scr/lessons/l24.js
@@ -0,0 +1,14 @@
+// Lesson 24: Destructuring
+
+console.log("Lesson 24: Destructuring");
+
+const userNameData = ["Ina", "Pina"];
+
+// index to access data
+const fiNa = userNameData[0];
+const fiNa2 = userNameData[1];
+
+// Shortening above data
+const [fina2, fina3] = ["fina", "mina"];
+console.log(fina2);
+console.log(fina3);
