6217f9ea69abb45a1c6ad7b9528ad6ff2acea966
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-18 06:32:10 +0000

    🔢

--- assets/lessonsUJS/l145.js
+++ assets/lessonsUJS/l145.js
@@ -4,13 +4,22 @@
 function wr11(tx1) {
   console.log(
     `%c
-        ---
-        ${tx1}
-        ---
+---
+${tx1}
+---
         `,
     "color:#0ea5e9; font-size: 1rem"
   );
 }
 
 wr11("Array Looping");
-const mvm1 = [200, 450, -400, 3000, -650, -130, 0, 130];
+const mvm1 = [200, 450, -400, 3000, -650, -130, 0, 130, 267, 300];
+
+// Using an for off loop to identify elements in the array
+for (const m of mvm1) {
+  if (m > 0) {
+    console.log(`Credit ${m}`);
+  } else {
+    console.log(`Debit ${m}`);
+  }
+}
