43cab690e66e5e2ce5f7038d2f75a3638883bc74
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-18 13:33:19 +0000

    🔢

---
+++ assets/lessonsUJS/l150.js
@@ -0,0 +1,14 @@
+// 150 : Map Filter and Reduce
+// Map loops over array and makes anew array
+
+// === utility function
+function wr11(tx1) {
+  console.log(
+    `%c
+    ---
+    ${tx1}
+    ---
+            `,
+    "color:#0ea5e9; font-size: 1rem"
+  );
+}
