41e081f0b4ea8996350a7305803f44aea58684cf
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-18 01:13:45 +0000

    🔢

--- assets/scr/lessons/l27.js
+++ assets/scr/lessons/l27.js
@@ -1,2 +1,31 @@
 // Lesson 27 0 Control Control Stuctures
 console.log("What is that");
+
+console.log("if Staements");
+
+// general structure
+if (10 === 10) {
+  // ..
+} else if (5 === 5) {
+  // ..
+} else if (2 === 2) {
+  //
+} else {
+}
+
+console.log(`
+---
+Actual Testing here 
+--
+`);
+
+// WebApi prompt
+const pwd = prompt("WRite Pwd");
+
+if (pwd === "Hi") {
+  alert("Right");
+} else if (pwd === "hi") {
+  alert("▶️ ALMOST");
+} else {
+  alert("😡😡😡");
+}
