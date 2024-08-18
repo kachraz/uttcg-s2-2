15081f52649eef5836c19d282ef7ba3d3ed5eafb
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-18 01:36:40 +0000

    🔢

--- assets/scr/lessons/l29fv.js
+++ assets/scr/lessons/l29fv.js
@@ -20,4 +20,17 @@ setTimeout(handleTimeOut())
     - In this case the 'setTimeout' will call the 'handleTimeOut
 */
 
-setTimeout(handleTimeOut);
+setTimeout(handleTimeOut, 1000);
+setTimeout(handleTimeOut, 2000);
+
+// here is an exaxmple of an anonymous function
+setTimeout(() => {
+  console.log("AnonFunc Timeout");
+}, 3000);
+
+// Building you own fucntion
+
+function greeter(grFn) {
+  grFn();
+}
+greeter(() => console.log("Hey"));
