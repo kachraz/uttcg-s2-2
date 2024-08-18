b22218a7150af389afa6ccff9ec1040b3a6d7459
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-18 01:25:10 +0000

    🔢

--- assets/scr/lessons/l29fv.js
+++ assets/scr/lessons/l29fv.js
@@ -1,1 +1,11 @@
+const { addSyntheticLeadingComment } = require("typescript");
+const { getAllJSDocTagsOfKind } = require("typescript");
+
 // Lessson 29: Functions as values
+00001223;
+getAllJSDocTagsOfKind
+
+addSyntheticLeadingComment
+000
+00
+02121OOO
