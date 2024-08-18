4bd8592bcf59cad797daeefb1acac6e20bde9b28
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-18 06:15:17 +0000

    🔢

---
+++ assets/lessonsUJS/145.js
@@ -0,0 +1,13 @@
+// 145: For each method
+
+// === utility function
+function wr11(tx1) {
+  console.log(
+    `%c
+        ---
+        ${tx1}
+        ---
+        `,
+    "color:#0ea5e9; font-size: 1rem"
+  );
+}
