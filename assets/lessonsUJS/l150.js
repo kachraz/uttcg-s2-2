cde1d4b06d8e057c9d50dc0150bddd3a6c8d4a88
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-19 17:51:37 +0000

    🔢

--- assets/lessonsUJS/l150.js
+++ assets/lessonsUJS/l150.js
@@ -12,3 +12,23 @@ function wr11(tx1) {
     "color:#0ea5e9; font-size: 1rem"
   );
 }
+
+wr11("Arrays Methods - Map Filter Reduce");
+
+// Working with the Data from JS
+const mvm1 = [200, 450, -400, 3000, -650, -130, 20, 130, 267, 300];
+
+// Converting the above usd -> eur
+const eu2USD = 1.1;
+
+const mvCon = mvm1.map(function (m) {
+  return m * eu2USD;
+});
+
+console.log(mvCon);
+
+// Writing the above with for of loop
+const mvCo2 = [];
+wr11("Using for method");
+for (const m of mvm1) mvCo2.push(m * eu2USD);
+console.log(mvCo2);
