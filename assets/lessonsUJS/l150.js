4c1b80f402f77af4b76cc57f0a40247099cc7fb4
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-19 18:06:56 +0000

    🔢

--- assets/lessonsUJS/l150.js
+++ assets/lessonsUJS/l150.js
@@ -18,17 +18,29 @@ wr11("Arrays Methods - Map Filter Reduce");
 // Working with the Data from JS
 const mvm1 = [200, 450, -400, 3000, -650, -130, 20, 130, 267, 300];
 
-// Converting the above usd -> eur
-const eu2USD = 1.1;
+// // Converting the above usd -> eur
+// const eu2USD = 1.1;
 
-const mvCon = mvm1.map(function (m) {
-  return m * eu2USD;
-});
+// const mvCon = mvm1.map(function (m) {
+//   return m * eu2USD;
+// });
 
-console.log(mvCon);
+// console.log(mvCon);
 
-// Writing the above with for of loop
-const mvCo2 = [];
-wr11("Using for method");
-for (const m of mvm1) mvCo2.push(m * eu2USD);
-console.log(mvCo2);
+// // Writing the above with for of loop
+// const mvCo2 = [];
+// wr11("Using for method");
+// for (const m of mvm1) mvCo2.push(m * eu2USD);
+// console.log(mvCo2);
+
+// // usign arrow functions
+// wr11("Using Arrow Function");
+// const mvC3 = mvm1.map((m) => m * eu2USD);
+// console.table(mvC3);
+
+// map methods - Index and array
+wr11("Using More map methods ");
+const mvDesc = mvm1.map(
+  (m, i, a) => `Mov ${i + 1}:  ${m > 0 ? "✅" : "❌"}  ${Math.abs(m)}`
+);
+console.log(mvDesc);
