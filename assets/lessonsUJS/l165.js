6726fe24a592e32f04c84918cccd3995f65a4bd6
Kzianon <110726496+kachraz@users.noreply.github.com>
2024-08-20 01:21:40 +0000

    🔢

--- assets/lessonsUJS/l165.js
+++ assets/lessonsUJS/l165.js
@@ -25,3 +25,26 @@ console.log(mr1);
 // Generating arrays automatically
 let mr2 = new Array(7); // Creati ng an empty array with 7 position
 console.log(mr2);
+
+// Fill method on array
+mr2.fill(69);
+console.log(mr2);
+
+// Making a new array from existing array
+let mr4 = Array.from({ length: 71 }, () => 1.4);
+console.log(mr4);
+
+// making ana array using index
+let mr5 = Array.from({ length: 6 }, (_, i) => i + 1);
+console.log(mr5);
+
+// creating a dice roll
+
+let diceRoll1 = Array.from({ length: 10 }, () => Math.random(20));
+console.log(diceRoll1);
+
+const diceRoll2 = Array.from(
+  { length: 10 },
+  () => Math.floor(Math.random() * 6) + 1
+);
+console.log(diceRoll2);
